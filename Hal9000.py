7f98c7b0d0d366e19c4b99cdb4a931b12443b162
Jib <jibbz2009@hotmail.co.uk>
2018-10-22 13:11:39 +0100

    Removed individual client commands utilised else ifs as the individual client commands meant that the bot was only registering the final command.
    Signed-off-by: Jib <jibbz2009@hotmail.co.uk>

--- Hal9000.py
+++ Hal9000.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 import asyncio
 import time
 
-Client = discord.Client ()
-client = commands.Bot (command_prefix = "!")
+Client = discord.Client()
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
@@ -16,14 +16,14 @@ async def on_message(message):
     if message.content == "cookie":
         await client.send_message(message.channel, ":cookie:")
 
-@client.event
-async def on_message(message):
-    if message.content == "100":
+    elif message.content == "100":
         await client.send_message(message.channel, ":100:")
 
-@client.event
-async def on_message(message):
-    if message.content == "god save the queen":
+    elif message.content == "hello":
+        await client.send_message(message.channel, "Welcome!")
+
+
+    elif message.content == "god save the queen":
         await client.send_message(message.channel, ":flag_gb: :flag_gb: :flag_gb:")
 
 client.run("NTAzNjU2MTc3NjgzMzk4Njc1.Dq5txg.WDWMGBovm-6VOTP86Nipck5YOcw")
