5ee71403daf2ae98748bd6ff51d2cc3f6dfc9c27
Khalif47 <khalif47@coventry.ac.uk>
2018-10-29 14:12:23 +0000

    removed redundant code
    Signed-off-by: Khalif47 <Khalif47@coventry.ac.uk>

--- Hal9000.py
+++ Hal9000.py
@@ -29,21 +29,6 @@ async def on_message(message):
         userID = message.author.id
         await client.send_message(message.channel, "<@%s> Hello Human" % (userID), tts=True)
 
-    @client.event
-    async def on_message(message):
-        # we do not want the bot to reply to itself
-        if message.author == client.user:
-            return
 
-        if message.content.startswith('!hello'):
-            msg = 'Hello {0.author.mention}'.format(message)
-            await client.send_message(message.channel, msg)
-
-    @client.event
-    async def on_ready():
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('------')
 client.run("NTAzNjU2MTc3NjgzMzk4Njc1.Dq5txg.WDWMGBovm-6VOTP86Nipck5YOcw")
 
