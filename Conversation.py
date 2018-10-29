17195bc8b3fa7c6a5fbf7cca4c98c574e2c89b8c
Mohamed Alhawaj (alhawajm) <alhawajm@coventry.ac.uk>
2018-10-29 14:04:27 +0000

    Create Conversation.py

---
+++ Conversation.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext.commands import bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix="!")
+
+
+@client.event
+async def on_ready():
+    print("Hello Dave")
+
+
+@client.event
+async def on_message(message):
+    if message.content == "how are you":
+        await client.send_message(message.channel, "Doing well. What about you?")
+    elif message.content == "great":
+        await client.send_message(message.channel, "Good to know! What are you up to today?")
+    elif message.content == "I have a project presentation":
+        await client.send_message(message.channel, "That's nice. Any way i can assist you in?")
+    elif message.content == "no i'm good":
+        await client.send_message(message.channel, "Alright then. catch you later.")
+
+
+@client.event
+async def on_message(message):
+        # we do not want the bot to reply to itself
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('!hello'):
+            msg = 'Hello {0.author.mention}'.format(message)
+            await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+client.run("NTAzNjU2MTc3NjgzMzk4Njc1.Dq5txg.WDWMGBovm-6VOTP86Nipck5YOcw")
