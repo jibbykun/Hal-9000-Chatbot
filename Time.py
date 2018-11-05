8f6ce9549c24af73197fa02dabae993d9c64d6c7
Jibraan Keyani (keyanij) <keyanij@coventry.ac.uk>
2018-11-05 01:06:46 +0000

    Gives a random response to make it fulfill the 'chatbot' criteria better.
    Signed-off-by: Jib <jibbz2009@hotmail.co.uk>

--- Time.py
+++ Time.py
@@ -1,9 +1,7 @@
 import discord
-from discord.ext.commands import bot
 from discord.ext import commands
-import asyncio
-import time
-import datetime
+import datetime  #built in datetime function used for this script
+import random #for the responses
 
 Client = discord.Client()
 client = commands.Bot(command_prefix="!")
@@ -13,8 +11,12 @@ currentDT = datetime.datetime.now()
 
 @client.event
 async def on_message(message):
-    timecheck=message.content
+    timecheck = message.content
     if "time" in timecheck:
         await client.send_message(message.channel, currentDT)
+        responses = ("Anything else?", "What else can help you with?", "I hope that was useful", "A good day to be productive!")
+        await client.send_message(message.channel, random.choice(responses))
+
+
 
 client.run("NTAzNjU2MTc3NjgzMzk4Njc1.Dq5txg.WDWMGBovm-6VOTP86Nipck5YOcw")
