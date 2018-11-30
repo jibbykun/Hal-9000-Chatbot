5e16ae329db265f7db8491f1ef33c4d5bbc99fe2
Jibraan Keyani (keyanij) <keyanij@coventry.ac.uk>
2018-11-30 01:24:38 +0000

    added help script text
    Signed-off-by: Jib <jibbz2009@hotmail.co.uk>

--- help.py
+++ help.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
-    print("Hello Dave")
+    print("Help script ready")
 
 @client.event
 async def on_message(message):
@@ -16,7 +16,12 @@ async def on_message(message):
 
     if message.content == "!HELP":
 
-        commands = ("If you type coventry university or coventry uni a script about the coventry university buildings will run!", "If you type can you tell me a joke? a script that tells jokes will run!")
+        commands = ("""
+        If you type coventry university or coventry uni a script about the coventry university buildings will run! 
+        If you type can you tell me a joke? a script that tells jokes will run! 
+        mention the weather and i can give you the weather forecast of Coventry!
+        mention 'time' and i'll give you updated time and date information!
+        """)
 
         await client.send_message(message.channel, "Here is a list of commands: ")
 
